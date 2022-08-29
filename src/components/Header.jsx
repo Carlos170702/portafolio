df4630f156b42aa1f407bd662f10b8ab348b3b39
Carlos170702 <carloscruz1dev@gmail.com>
2022-08-29 09:16:44 -0500

    29/08/2022 editar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export const Header = () => {
                     <h1 className="header__titulo">Hi! I'm carlos Web Design </h1>
                     <p className="header__about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium consequuntur doloribus aut, vero quidem eos labore, esse repudiandae sunt hic dolorum ut reprehenderit voluptates nulla facere reiciendis pariatur amet eum eaque necessitatibus perspiciatis. Adipisci deserunt iure ad, eaque molestias consectetur facere vero, reiciendis veniam ab sit! Sequi repellendus, officiis obcaecati quas beatae dolore mollitia,</p>
                     <button
-                    className='header__btn'
+                        className='header__btn'
                         type='submit'
                     >
                         let's Connect    <FiArrowRightCircle />
@@ -19,7 +19,12 @@ export const Header = () => {
                 </div>
 
                 <div className="herader__pefil">
-                    <div className="header__img"></div>
+                    <div className="header__card">
+                        <div className="header__imgBx">
+                            <img className="header__img" src='https://www.elsoldemexico.com.mx/doble-via/xoiaw8-cuales-son-las-razas-de-gato-mas-grandes-del-mundo.jpg/ALTERNATES/LANDSCAPE_768/%C2%BFCua%CC%81les%20son%20las%20razas%20de%20gato%20ma%CC%81s%20grandes%20del%20mundo.jpg' />
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
