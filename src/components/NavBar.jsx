2ba5b054ca3e59c90355a2238630ed702d9f5d73
Carlos170702 <108914307+Carlos170702@users.noreply.github.com>
2022-09-06 08:25:48 -0500

    06/09/22

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,5 @@
+import { FiAlignJustify } from 'react-icons/fi'
+
 export const NavBar = ({ links }) => {
     return (
         <>
@@ -5,7 +7,7 @@ export const NavBar = ({ links }) => {
                 <div className="nav__img">
                     <a href="#home"><img src="https://firebasestorage.googleapis.com/v0/b/portafolio-c6563.appspot.com/o/logo.png?alt=media&token=42f4cd2d-e548-4a1d-abc3-32b08e99182d" alt="Logo" className="nav__logo" /></a>
                 </div>
-        
+
                 <ul className="nav__list">
                     {
                         links.map(item => (
@@ -15,6 +17,9 @@ export const NavBar = ({ links }) => {
                         ))
                     }
                 </ul>
+                <div className="nav__hamburger">
+                    <FiAlignJustify />
+                </div>
             </nav>
         </>
     )
